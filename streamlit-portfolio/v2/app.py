e9dc4bcd25be50751e9ce6e6e54851120f33de32
Bruno M. Guerreiro <33891979+cryobiochem@users.noreply.github.com>
2024-12-06 12:26:56 +0000

    Update app.py

--- streamlit-portfolio/v2/app.py
+++ streamlit-portfolio/v2/app.py
@@ -1,11 +1,4 @@
 import streamlit as st 
-import numpy as np
-import pandas as pd
-import plotly.express as px
-import plotly.graph_objects as go
-import openpyxl
-import base64
-from plotly.subplots import make_subplots
 
 ### METADATA
 st.set_page_config(
